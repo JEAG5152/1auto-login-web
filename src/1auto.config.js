36af0489b6f4550f908899029761202b714db782
update 2023-11-30 <>
2024-06-11 23:54:43 -0600

    change host

--- src/1auto.config.js
+++ src/1auto.config.js
@@ -1,21 +1,21 @@
 const configs = {
 	api: {
 		local: {
-			url: 'http://localhost:4500/unauto',
+			url: 'http://localhost:4501/unauto',
             headers: {
                 'content-type': 'application/json',
                 'authorization': 'Bearer $argon2id$v=19$m=65536,t=3,p=4$G3pGLttD87t12qh7Ec3DKg$atkP9EthtJS2BeE7/HJVb0raeqd453x/Yjs5Q4kxoOQ',
             }
 		},
 		qa: {
-			url: 'http://api-qa.1auto.mx/unauto',
+			url: 'http://api-qa-login.1auto.mx/unauto',
             headers: {
                 'content-type': 'application/json',
                 'authorization': 'Bearer $argon2id$v=19$m=65536,t=3,p=4$G3pGLttD87t12qh7Ec3DKg$atkP9EthtJS2BeE7/HJVb0raeqd453x/Yjs5Q4kxoOQ',
             }
 		},
 		production: {
-			url: 'https://api-qa.1auto.mx/unauto',
+			url: 'https://api-qa-login.1auto.mx/unauto',
             headers: {
                 'content-type': 'application/json',
                 'authorization': 'Bearer $argon2id$v=19$m=65536,t=3,p=4$G3pGLttD87t12qh7Ec3DKg$atkP9EthtJS2BeE7/HJVb0raeqd453x/Yjs5Q4kxoOQ',
