cf160f6541bc53edca8a61ea4a1a681a12f96b81
update 2023-11-30 <>
2024-07-09 00:29:12 -0600

    change host

--- src/1auto.config.js
+++ src/1auto.config.js
@@ -8,14 +8,14 @@ const configs = {
             }
 		},
 		qa: {
-			url: 'http://api-qa-login.1auto.mx/unauto',
+			url: 'http://api-qa-login.1auto.com.mx/unauto',
             headers: {
                 'content-type': 'application/json',
                 'authorization': 'Bearer $argon2id$v=19$m=65536,t=3,p=4$G3pGLttD87t12qh7Ec3DKg$atkP9EthtJS2BeE7/HJVb0raeqd453x/Yjs5Q4kxoOQ',
             }
 		},
 		production: {
-			url: 'https://api-qa-login.1auto.mx/unauto',
+			url: 'https://api-qa-login.1auto.com.mx/unauto',
             headers: {
                 'content-type': 'application/json',
                 'authorization': 'Bearer $argon2id$v=19$m=65536,t=3,p=4$G3pGLttD87t12qh7Ec3DKg$atkP9EthtJS2BeE7/HJVb0raeqd453x/Yjs5Q4kxoOQ',
